5ad6fec0302237ae6975f70acdd13770f02961d6
Akatsuki2555 <59791284+Akatsuki2555@users.noreply.github.com>
2024-06-24 14:29:40 +0200

    Ability to spawn multiple plushies (#2)
    * Attempt 1 at multiple plushies at once :3
    
    * Attempt 2 (actually load the prefab)
    
    * Attempt 3 fix spawning
    
    * Register plushies
    
    * Update version

--- AstolfoBeanPlushie/AstolfoBeanPlushie/AstolfoBeanPlushie.cs
+++ AstolfoBeanPlushie/AstolfoBeanPlushie/AstolfoBeanPlushie.cs
@@ -11,7 +11,7 @@ namespace AstolfoBeanPlushie
     {
         public override string ID => "AstolfoBeanPlushie";
 
-        public override string Version => "0.1";
+        public override string Version => "0.3";
 
         public override string Author => "アカツキ";
 
@@ -23,33 +23,131 @@ namespace AstolfoBeanPlushie
             SetupFunction(Setup.OnSave, Mod_OnSave);
         }
 
+        private List<GameObject> _plushies = new List<GameObject>();
         private GameObject _plushie;
+        private GameObject _plushiePrefab;
+
+        public override void ModSettings()
+        {
+            base.ModSettings();
+
+            Settings.AddButton(this, "Spawn Plushie", () =>
+            {
+                var player = GameObject.Find("PLAYER");
+                SpawnPlushie(player.transform.position, player.transform.rotation);
+            });
+
+            Settings.AddButton(this, "Spawn 5 Plushies", () =>
+            {
+                var player = GameObject.Find("PLAYER");
+                for (int i = 0; i < 5; i++)
+                {
+                    SpawnPlushie(player.transform.position, player.transform.rotation);
+                }
+            });
+
+            Settings.AddButton(this, "Spawn 10 Plushies", () =>
+            {
+                var player = GameObject.Find("PLAYER");
+                for (int i = 0; i < 10; i++)
+                {
+                    SpawnPlushie(player.transform.position, player.transform.rotation);
+                }
+            });
+
+            Settings.AddButton(this, "Delete All Plushies", () =>
+            {
+                foreach (var item in _plushies)
+                {
+                    GameObject.Destroy(item);
+                }
+
+                _plushies.Clear();
+            });
+        }
+
+        private GameObject SpawnPlushie(Vector3 pos, Quaternion rot)
+        {
+            _plushie = GameObject.Instantiate(_plushiePrefab);
+            _plushie.AddComponent<Rigidbody>();
+            _plushie.MakePickable();
+            _plushie.name = "Astolfo Plushie(Clone)";
+            _plushie.transform.position = pos;
+            _plushie.transform.rotation = rot;
+            _plushies.Add(_plushie);
+            return _plushie;
+        }
 
         private void Mod_Load()
         {
             var assetBundle = AssetBundle.CreateFromMemoryImmediate(Resource1.astolfoplushie);
+            _plushiePrefab = assetBundle.LoadAsset<GameObject>("Astolfo");
+            assetBundle.Unload(false);
 
-            var beanPlushie = assetBundle.LoadAsset<GameObject>("Astolfo");
-            _plushie = GameObject.Instantiate(beanPlushie);
-            _plushie.AddComponent<Rigidbody>();
-            _plushie.MakePickable();
-            _plushie.name = "Astolfo Plushie(Clone)";
             var player = GameObject.Find("PLAYER");
-            _plushie.transform.position = player.transform.position;
 
-            if (SaveLoad.ValueExists(this, "plushiePos") && SaveLoad.ValueExists(this, "plushieRot"))
+            if (SaveLoad.ValueExists(this, "plushies"))
             {
-                _plushie.transform.position = SaveLoad.ReadValue<Vector3>(this, "plushiePos");
-                _plushie.transform.rotation = SaveLoad.ReadValue<Quaternion>(this, "plushieRot");
+                var plushies = SaveLoad.DeserializeClass<SavedPlushies>(this, "plushies");
+                foreach (var plushie in plushies.Plushies)
+                {
+                    SpawnPlushie(plushie.Position, plushie.Rotation);
+                }
             }
 
-            assetBundle.Unload(false);
+            // Migrate from previous commit
+            if (SaveLoad.ValueExists(this, "plushiePos") && SaveLoad.ValueExists(this, "plushieRot"))
+            {
+                var pos = SaveLoad.ReadValue<Vector3>(this, "plushiePos");
+                var rot = SaveLoad.ReadValue<Quaternion>(this, "plushieRot");
+                SpawnPlushie(pos, rot);
+
+                SaveLoad.DeleteValue(this, "plushiePos");
+                SaveLoad.DeleteValue(this, "plushieRot");
+            }
+
         }
 
         private void Mod_OnSave()
         {
-            SaveLoad.WriteValue<Vector3>(this, "plushiePos", _plushie.transform.position);
-            SaveLoad.WriteValue<Quaternion>(this, "plushieRot", _plushie.transform.rotation);
+            var saved = new List<SavedPlushie>();
+            foreach (var plushie in _plushies)
+            {
+                saved.Add(new SavedPlushie(plushie.transform.position, plushie.transform.rotation));
+            }
+
+            SaveLoad.SerializeClass<SavedPlushies>(this, new SavedPlushies(saved), "plushies");
+        }
+    }
+
+    [Serializable]
+    public class SavedPlushies
+    {
+        public List<SavedPlushie> Plushies;
+
+        public SavedPlushies()
+        {
+            Plushies = new List<SavedPlushie>();
+        }
+
+        public SavedPlushies(List<SavedPlushie> toImport)
+        {
+            Plushies = new List<SavedPlushie>();
+            Plushies.AddRange(toImport);
+        }
+    }
+
+    [Serializable]
+    public class SavedPlushie
+    {
+        public Vector3 Position { get; set; }
+        public Quaternion Rotation { get; set; }
+
+        public SavedPlushie() { }
+        public SavedPlushie(Vector3 position, Quaternion rotation)
+        {
+            Position = position;
+            Rotation = rotation;
         }
     }
 }
